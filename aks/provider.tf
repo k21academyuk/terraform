30f268d00ac19f859542eb13ce8d8727b3043d6b
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 12:51:24 +0530

    Update provider.tf

--- aks/provider.tf
+++ aks/provider.tf
@@ -1,12 +1,18 @@
+# Define the Azure provider configuration block.
 provider "azurerm" {
+  # Configure provider features, if any.
   features {}
 }
+
+# Declare the Terraform configuration block specifying required provider versions.
 terraform {
+  # Specify the required provider and its version.
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.66.0"
     }
   }
+  # Specify the minimum required Terraform version.
   required_version = ">= 0.14"
 }
