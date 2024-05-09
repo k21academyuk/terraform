5261c6910cac3ef68578640b87ce33211369d3c9
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 12:49:41 +0530

    Update aks-cluster.tf

--- aks/aks-cluster.tf
+++ aks/aks-cluster.tf
@@ -1,18 +1,22 @@
+# Define an Azure resource group with the specified name and location.
 resource "azurerm_resource_group" "default" {
   name     = "azure-rg"
   location = "West US 2"
   
+  # Set tags for the resource group to categorize it under a specific environment.
   tags = {
     environment = "Demo"
   }
 }
 
+# Define an Azure Kubernetes Service (AKS) cluster within the previously defined resource group.
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "azure-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "azure-k8s"
+  location            = azurerm_resource_group.default.location  # Use the location of the resource group
+  resource_group_name = azurerm_resource_group.default.name      # Use the name of the resource group
+  dns_prefix          = "azure-k8s"                              # DNS prefix for the AKS cluster
   
+  # Configure the default node pool for the AKS cluster.
   default_node_pool {
     name            = "default"
     node_count      = 2
@@ -20,15 +24,18 @@ resource "azurerm_kubernetes_cluster" "default" {
     os_disk_size_gb = 30
   }
   
+  # Configure service principal for managing the AKS cluster.
   service_principal {
-    client_id     = var.appId
-    client_secret = var.password
+    client_id     = var.appId       # Assign the client ID for authentication
+    client_secret = var.password     # Assign the client secret for authentication
   }
   
+  # Enable role-based access control (RBAC) for the AKS cluster.
   role_based_access_control {
     enabled = true
   }
   
+  # Set tags for the AKS cluster to categorize it under a specific environment.
   tags = {
     environment = "Demo"
   }
