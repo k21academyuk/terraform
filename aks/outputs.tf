ec3ec4e00615dc1e303a66a434b441b2cbc624cb
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 12:50:34 +0530

    Update outputs.tf

--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,7 +1,9 @@
+# Output the name of the Azure resource group created earlier.
 output "resource_group_name" {
-  value = azurerm_resource_group.default.name
+  value = azurerm_resource_group.default.name  # Retrieve the name of the resource group
 }
 
+# Output the name of the Azure Kubernetes Service (AKS) cluster created earlier.
 output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.default.name
+  value = azurerm_kubernetes_cluster.default.name  # Retrieve the name of the AKS cluster
 }
