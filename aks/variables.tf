8653b9b118bad9680fd40f966d13822b215912e5
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-05-09 12:51:56 +0530

    Update variables.tf

--- aks/variables.tf
+++ aks/variables.tf
@@ -1,7 +1,9 @@
+# Define a variable for the Azure Kubernetes Service (AKS) Cluster service principal ID.
 variable "appId" {
   description = "Azure Kubernetes Service Cluster service principal"
 }
 
+# Define a variable for the Azure Kubernetes Service (AKS) Cluster password.
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
 }
